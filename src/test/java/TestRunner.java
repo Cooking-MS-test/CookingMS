21563c6959643c04d201bac5b32e55f80499ac6f
ZahiQudu <zahi.qudu8@gmail.com>
2025-03-15 15:01:14 +0200

    Adding Project files

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,3 +1,4 @@
+import io.cucumber.junit.CucumberOptions.SnippetType;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
@@ -5,8 +6,11 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = ".idea/Features",
-        glue = "stepDefinitions",
-        plugin = {"pretty", "html:target/cucumber-reports.html"}
+        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        monochrome = true,
+        snippets = SnippetType.CAMELCASE ,
+        glue = "stepDefinitions"
+
 )
 public class TestRunner {
 }
